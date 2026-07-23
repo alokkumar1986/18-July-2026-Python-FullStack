8c10321074c3f2ac727e8871c5e4412f40e96628
Alok Nayak <alok.nayak@iserveu.in>
2026-07-23 10:05:20 +0530

    '23-07-20256'

---
+++ 01.py
@@ -0,0 +1,16 @@
+#Write a program to add two numbers
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = a + b
+print("The sum is: ",c)
+
+
+
+#write a program to swap two numbers
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = a
+a = b
+b = c
+print("The first number is: ",a)
+print("The second number is: ",b)
